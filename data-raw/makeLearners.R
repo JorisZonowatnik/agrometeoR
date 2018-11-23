0440e1dca3bc47e6f644f8447b01193240df18d1
pokyah <thomas.goossens@protonmail.com>
2018-11-23 11:06:42 +0000

    learners added. Must be document in a data section !

---
+++ data-raw/makeLearners.R
@@ -0,0 +1,74 @@
+learners = list(
+  # defining the simple learners
+  lrn.lm.alt_x_y = makeFilterWrapper(
+    learner = makeLearner(
+      cl = "regr.lm",
+      id = "multiReg.alt_x_y",
+      predict.type = 'se'),
+    fw.method = "linear.correlation",
+    fw.mandatory.feat = c("elevation", "y", "x"),
+    fw.abs = 3),
+
+  # this learner does not support missing values. So we input these
+  # lrn.lm.alt_x_y = makeImputeWrapper(
+  #   lrn.lm.alt_x_y,
+  #   cols = list(tsa_hp1 = imputeMedian()))
+
+  lrn.gstat.idw = makeLearner(
+    cl = "regr.gstat",
+    id = "idw",
+    predict.type = "se"),
+
+  lrn.gstat.ts1 = makeLearner(
+    cl = "regr.gstat",
+    id = "ts1",
+    par.vals = list(degree = 1),
+    predict.type = "se"),
+
+  lrn.gstat.ts2 = makeLearner(
+    cl = "regr.gstat",
+    id = "ts2",
+    par.vals = list(degree = 2),
+    predict.type = "se"),
+
+  lrn.gstat.ok = makeFilterWrapper(
+    learner = makeLearner(
+      cl = "regr.gstat",
+      id = "ok",
+      par.vals = list(
+        range = 800,
+        psill = 200000,
+        model.manual = "Sph",
+        nugget = 0),
+      predict.type = "se"),
+    fw.method = "linear.correlation",
+    fw.mandatory.feat = c("y", "x"),
+    fw.abs = 2),
+
+  lrn.gstat.ked = makeFilterWrapper(
+    learner = makeLearner(
+      cl = "regr.gstat",
+      id = "ked",
+      par.vals = list(
+        range = 800,
+        psill = 200000,
+        model.manual = "Sph",
+        nugget = 0),
+      predict.type = "se"),
+    fw.method = "linear.correlation",
+    fw.mandatory.feat = c("y", "x", "elevation"),
+    fw.abs = 3),
+
+  lrn.gstat.1nn = makeFilterWrapper(
+    learner = makeLearner(
+      cl = "regr.gstat",
+      id = "nn1",
+      par.vals = list(
+        nmax = 1),
+      predict.type = "se"),
+    fw.method = "linear.correlation",
+    fw.mandatory.feat = c("y", "x"),
+    fw.abs = 2)
+)
+
+devtools::use_data(learners, internal = FALSE, overwrite = TRUE)
