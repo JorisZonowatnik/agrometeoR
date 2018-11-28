7db02b2126986dd8ebe60ab3c5ef1cebec5db7f2
pokyah <thomas.goossens@protonmail.com>
2018-11-28 11:41:18 +0000

    simplyfing tasks name creation

--- R/makeTasks.R
+++ R/makeTasks.R
@@ -3,29 +3,36 @@
 #' @author Thomas Goossens
 #' @param stations a character specifying the sid's of the stations to use separated by commas
 #' @param dataset a dataframe containing all the hourly/daily records you want to transform to a list of mlr tasks
-#' @param target a charchter specifying the neame of the target variable
+#' @param target a charchter specifying the name of the target variable
 #' @param drop a character vector specifying the explanatory variables you wan to drop.
 #' @return an object of class list containing elements of class mlr::makeRegrTask()
 makeTasks <- function(
   dataset,
-  drop = c("sid", "mtime"),
+  drop = NULL,
   target
 ){
 
-  # group by mtime and make lists of dataframes
-  dataset = split(dataset, dataset$mtime)
+  # dataset renaming mtime vars for list grouping
+  dataset = dataset %>%
+    dplyr::mutate(task.id = gsub("[^[:digit:]]", "", mtime)) %>%
+    dplyr::select(-mtime)
+
+  # group by task.id and make lists of dataframes
+  dataset = split(
+    x = dataset,
+    f = dataset$task.id)
 
   # make machine learning regression tasks for each list element
   tasks = lapply(seq_along(dataset), function(d)
     mlr::dropFeatures(
       task = mlr::makeRegrTask(
-        data = dataset[[d]] %>%
-          dplyr::mutate_at("mtime", as.numeric),
+        data = dataset[[d]] %>% dplyr::select(-c(task.id, sid)),
         target = target,
-        id = gsub(":", "", names(dataset)[d])),
+        id = names(dataset)[d]),
       features = drop)
     )
-  names(tasks) = gsub(":", "", names(dataset))
+
+  names(tasks) = names(dataset)
 
   # summarise the dataset
   # insights = summary(mlr::getTaskData(task))
