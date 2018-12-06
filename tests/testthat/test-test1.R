f6aeb13a53f35f56781350a413ecf3c10db40174
pokyah <thomas.goossens@protonmail.com>
2018-12-06 10:24:40 +0000

    implementing examplative objects that can be loaded with data()

--- tests/testthat/test-test1.R
+++ tests/testthat/test-test1.R
@@ -1,3 +1,15 @@
+# testthat::test_file("./tests/testthat/test_spatial_gstat.R")
+# devtools::test(filter = "gstat")
+# devtools::document()
+# devtools::load_all()
+# devtools::test(filter = "spatial_regr_gstat")
+# https://stackoverflow.com/questions/31548796/debugging-testthat-tests-in-rstudio
+# sink(NULL)
+# https://github.com/r-lib/devtools/issues/1675 - Error in x[[method]](...) : attempt to apply non-function
+# https://stackoverflow.com/questions/50083521/error-in-xmethod-attempt-to-apply-non-function-in-testthat-test-when
+# https://stackoverflow.com/questions/7028385/can-i-remove-an-element-in-dot-dot-dot-and-pass-it-on
+
+
 context("test-test1.R")
 
 test_that("multiplication works", {
