e7e069fc1a897c4681ea822bdeb3941bc44999b7
pokyah <thomas.goossens@protonmail.com>
2018-12-11 13:02:49 +0000

    first test implemented + geojson updated + script to create pcakage dataset

--- tests/testthat/helper_objects.R
+++ tests/testthat/helper_objects.R
@@ -1,2 +1,16 @@
 # creating the objects that will be used in the tests
 # all the objects can be loaded with data() as their were created in the data-raw folder of the present package
+
+# definition of the parameters used in the tests
+test_user_token = Sys.getenv("AGROMET_API_V1_KEY")
+test_bad_user_token = "mysterious_token"
+test_dfrom = "2017-03-04T15:00:00Z"
+test_bad_dfrom = "timea"
+test_dto = "2017-03-04T15:00:00Z"
+test_bad_dto = "timeb"
+test_stations = paste0(as.character(stations.df$sid), collapse = ",")
+test_bad_stations = "1,2,3,4,5,8"
+test_sensor = "tsa"
+test_bad_sensor = "foo"
+test_staticExpl = "elevation"
+test_bad_staticExpl = "blah"
---
+++ tests/testthat/test_makeDataset.R
@@ -0,0 +1,135 @@
+# testthat::test_file("./tests/testthat/test_spatial_gstat.R")
+# devtools::test(filter = "gstat")
+# devtools::document()
+# devtools::load_all()
+# devtools::test(filter = "spatial_regr_gstat")
+# https://stackoverflow.com/questions/31548796/debugging-testthat-tests-in-rstudio
+# sink(NULL)
+# https://github.com/r-lib/devtools/issues/1675 - Error in x[[method]](...) : attempt to apply non-function
+# https://stackoverflow.com/questions/50083521/error-in-xmethod-attempt-to-apply-non-function-in-testthat-test-when
+# https://stackoverflow.com/questions/7028385/can-i-remove-an-element-in-dot-dot-dot-and-pass-it-on
+# http://r-pkgs.had.co.nz/tests.html
+context("makeDataset")
+
+groups = list(
+  good = list(
+    list(
+      id = "good",
+      stations = test_stations,
+      user_token = test_user_token,
+      dfrom = test_dfrom,
+      dto = test_dto,
+      sensor = test_sensor,
+      staticExpl = test_staticExpl,
+      json = NULL,
+      dynExpl = NULL)
+    ),
+  bad = list(
+     list(
+      id = "bad_user_token",
+      stations = test_stations,
+      user_token = test_bad_user_token,
+      dfrom = test_dfrom,
+      dto = test_dto,
+      sensor = test_sensor,
+      staticExpl = test_staticExpl,
+      json = NULL,
+      dynExpl = NULL),
+    list(
+      id = "bad_stations",
+      stations = test_bad_stations,
+      user_token = test_user_token,
+      dfrom = test_dfrom,
+      dto = test_dto,
+      sensor = test_sensor,
+      staticExpl = test_staticExpl,
+      json = NULL,
+      dynExpl = NULL),
+    list(
+      id = "bad_dfrom",
+      stations = test_stations,
+      user_token = test_user_token,
+      dfrom = test_bad_dfrom,
+      dto = test_dto,
+      sensor = test_sensor,
+      staticExpl = test_staticExpl,
+      json = NULL,
+      dynExpl = NULL),
+    list(
+      id = "bad_dto",
+      stations = test_stations,
+      user_token = test_user_token,
+      dfrom = test_dfrom,
+      dto = test_bad_dto,
+      sensor = test_sensor,
+      staticExpl = test_staticExpl,
+      json = NULL,
+      dynExpl = NULL),
+    list(
+      id = "bad_sensor",
+      stations = test_stations,
+      user_token = test_user_token,
+      dfrom = test_dfrom,
+      dto = test_dto,
+      sensor = test_bad_sensor,
+      staticExpl = test_staticExpl,
+      json = NULL,
+      dynExpl = NULL),
+    list(
+      id = "bad_staticExpl",
+      stations = test_stations,
+      user_token = test_user_token,
+      dfrom = test_dfrom,
+      dto = test_dto,
+      sensor = test_sensor,
+      staticExpl = test_bad_staticExpl,
+      json = NULL,
+      dynExpl = NULL)))
+
+test_that("Output has the good structure, whatever error or not", {
+  for (group in 1:length(groups)) {
+    for (case in 1:length(group)) {
+      object = do.call(what = makeDataset, args = groups[[group]][[case]][-1])
+      expect_is(object, class = "list")
+      expect_length(object, 2)
+      expect_equal(names(object), c("bool", "output"))
+    }
+  }
+})
+
+test_that("Error are thrown when a bad parameter is passed", {
+  for (group in 1:length(groups)) {
+    if (names(groups[1]) == "bad") {
+      for (case in 1:length(group)) {
+        object = do.call(what = makeDataset, args = groups[[group]][[case]][-1])
+        expect_error(object)
+      }
+    }
+  }
+})
+
+test_that("When bool isTRUE (no error), output has class dataframe and when bool isFALSE, output has class NULL", {
+  for (group in 1:length(groups)) {
+      for (case in 1:length(group)) {
+        object = do.call(what = makeDataset, args = groups[[group]][[case]][-1])
+        if (isTRUE(object$bool)) {
+          expect_is(object$output$value, "data.frame")
+        } else {
+          expect_null(object$output$value)
+        }
+      }
+    }
+})
+
+test_that("When bool isTRUE, output has class dataframe and when bool isFALSE, output has class NULL", {
+  for (group in 1:length(groups)) {
+    for (case in 1:length(group)) {
+      object = do.call(what = makeDataset, args = groups[[group]][[case]][-1])
+      if (isTRUE(object$bool)) {
+        expect_is(object$output$value, "data.frame")
+      } else {
+        expect_null(object$output$value)
+      }
+    }
+  }
+})
