9426313f749b090a9601a6e69bfc67acc346bddb
pokyah <thomas.goossens@protonmail.com>
2018-12-11 14:16:17 +0000

    implementing various unit tests

---
+++ tests/testthat/test_makeModel.R
@@ -0,0 +1,65 @@
+
+library(testthat)
+context("Testing makeModel")
+
+groups = list(
+  good = list(
+    list(
+      id = "good.lm",
+      task = test_task,
+      learner = learners$lrn.lm.alt_x_y
+    ),
+    list(
+      id = "good.gstat",
+      task = test_task,
+      learner = learners$lrn.gstat.5nn
+    )),
+  bad = list(
+    list(
+      id = "bad_task",
+      task = test_bad_task,
+      learner = learners$lrn.gstat.5nn
+    ),
+    list(
+      id = "bad_learner",
+      task = test_task,
+      learner = "a_bad_learner"
+    )
+  ))
+
+test_that("Output has the good structure, whatever error or not", {
+  for (group in 1:length(groups)) {
+    for (case in 1:length(group)) {
+      object = do.call(what = makeModel, args = groups[[group]][[case]][-1])
+      expect_is(object, class = "list")
+      expect_length(object, 2)
+      expect_equal(names(object), c("bool", "output"))
+    }
+  }
+})
+
+test_that("Error are thrown when a bad parameter is passed", {
+  for (group in 1:length(groups)) {
+    if (names(groups[1]) == "bad") {
+      for (case in 1:length(group)) {
+        object = do.call(what = makeModel, args = groups[[group]][[case]][-1])
+        expect_error(object)
+      }
+    }
+  }
+})
+
+test_that("When bool isTRUE (no error), output has class RegrTask and when bool isFALSE, output has class NULL", {
+  for (group in 1:length(groups)) {
+    for (case in 1:length(group)) {
+      object = do.call(what = makeModel, args = groups[[group]][[case]][-1])
+      if (isTRUE(object$bool)) {
+        expect_is(object$output$value[[1]], "RegrTask")
+      } else {
+        expect_null(object$output$value)
+      }
+    }
+  }
+})
+
+
---
+++ tests/testthat/test_makeTasks.R
@@ -0,0 +1,63 @@
+
+library(testthat)
+context("Testing makeTask")
+
+groups = list(
+  good = list(
+    list(
+      id = "good",
+      dataset = test_dataset,
+      drop = NULL,
+      target = "tsa"
+      )),
+  bad = list(
+    list(
+      id = "bad_target",
+      dataset = test_dataset,
+      drop = NULL,
+      target = "foo"
+    ),
+    list(
+      id = "bad_dataset",
+      dataset = test_bad_dataset,
+      drop = NULL,
+      target = "tsa"
+    )
+  ))
+
+test_that("Output has the good structure, whatever error or not", {
+  for (group in 1:length(groups)) {
+    for (case in 1:length(group)) {
+      object = do.call(what = makeTasks, args = groups[[group]][[case]][-1])
+      expect_is(object, class = "list")
+      expect_length(object, 2)
+      expect_equal(names(object), c("bool", "output"))
+    }
+  }
+})
+
+test_that("Error are thrown when a bad parameter is passed", {
+  for (group in 1:length(groups)) {
+    if (names(groups[1]) == "bad") {
+      for (case in 1:length(group)) {
+        object = do.call(what = makeTasks, args = groups[[group]][[case]][-1])
+        expect_error(object)
+      }
+    }
+  }
+})
+
+test_that("When bool isTRUE (no error), output has class RegrTask and when bool isFALSE, output has class NULL", {
+  for (group in 1:length(groups)) {
+    for (case in 1:length(group)) {
+      object = do.call(what = makeTasks, args = groups[[group]][[case]][-1])
+      if (isTRUE(object$bool)) {
+        expect_is(object$output$value, "RegrTask")
+      } else {
+        expect_null(object$output$value)
+      }
+    }
+  }
+})
+
+
