65a9c6c7ea867ce5acdd24889aa6f80c62ffee11
pokyah <thomas.goossens@protonmail.com>
2019-02-20 10:22:46 +0000

    fixing errors in unit test related to error detection. Test not passing anymore

--- tests/testthat/test_makeTask.R
+++ tests/testthat/test_makeTask.R
@@ -13,17 +13,18 @@ groups = list(
   bad = list(
     list(
       id = "bad_target",
-      dataset = test_dataset,
-      drop = NULL,
-      target = "foo"
-    ),
-    list(
-      id = "bad_dataset",
       dataset = test_bad_dataset,
       drop = NULL,
+      target = "foo"
+    )),
+  warning = list(
+    list(
+      id = "dataset_with_NA",
+      dataset = test_dataset_with_NA,
+      drop = NULL,
       target = "tsa"
-    )
-  ))
+    ))
+)
 
 test_that("Output has the good structure, whatever error or not", {
   for (group in 1:length(groups)) {
@@ -38,10 +39,23 @@ test_that("Output has the good structure, whatever error or not", {
 
 test_that("Error are thrown when a bad parameter is passed", {
   for (group in 1:length(groups)) {
-    if (names(groups[1]) == "bad") {
+    if (names(groups[group]) == "bad") {
       for (case in 1:length(group)) {
+        browser()
         object = do.call(what = makeTask, args = groups[[group]][[case]][-1])
         expect_error(object)
+      }
+    }
+  }
+})
+
+test_that("Warning is thrown when dataset contains NA values", {
+  for (group in 1:length(groups)) {
+    if (names(groups[group]) == "warning") {
+      for (case in 1:length(group)) {
+        browser()
+        object = do.call(what = makeTask, args = groups[[group]][[case]][-1])
+        expect_warning(object)
       }
     }
   }
