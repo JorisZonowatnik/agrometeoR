baf35fb452456954ff8cabd86e688debe6b8ccd9
pokyah <thomas.goossens@protonmail.com>
2019-03-12 15:22:05 +0000

    smarter and modualr makePlots

--- R/makePlots.R
+++ R/makePlots.R
@@ -3,23 +3,31 @@
 #' @author Thomas Goossens
 #' @import mlr
 #' @import ggplot2
+#' @importFrom lubridate hour
+#' @importFrom lubridate date
+#' @importFrom lubridate week
+#' @importFrom lubridate month
 #' @importFrom magrittr %>%
 #' @param records a list of hourly dataframes or a single dataframe ::TODO ::Must contain sunset and sunrise !
-#' @param sensor a character vector specifying the name of the sensors you want to get the descriptive statistics
-#' @param combs a named list which elements are chatacter vectors containing the sid's combinations of the stations you want to put on the same plot
-#' @param dfrom a character vector
-#' @param dto a character vector
+#' @param dfrom a character
+#' @param dto a character
+#' @param sensor (optional) a character vector specifying the name of the sensors you want to get the descriptive statistics.
+#' If not provided, the function will compute the plots for all the sensors available in your records dataset. These must be one of c("tsa", "plu", "ens", "vvt", "hct")
+#' @param combs (optional) a named list which elements are chatacter vectors containing the sid's combinations of the stations you want to put on the same plot.
+#' If not provided, the function will create a single plot for each sid and will not compute blandAltman and scatter
+#' @param grouping (optional) a character specifying how you want to group points on scatter and blandAltman plots. One of lubridate functions : "month", "week","as_date", "hour".
+#' if not provided, the function will choose the most appropriate according to the whole timeframe of your records
 #' @return a list
 #'
-#' records = makeDataset(dfrom = ex_dfrom, dto = ex_dto, sensor = c("tsa", "vvt"), stations = unique(stations.df$sid))
-
+#' records = makeDataset(dfrom = "2016-02-04T15:00:00Z", dto = "2016-03-04T15:00:00Z", sensor = c("tsa", "vvt"), stations = stations.df$sid)
 makePlots = function(
   records,
-  # sensors,
-  combs,
   dfrom = NULL,
   dto = NULL,
-  interactive = TRUE){
+  interactive = TRUE,
+  sensors = NULL,
+  combs = NULL,
+  grouping = NULL){
 
   # from list of df's to big df
   records = dataframizeRecords(records)
@@ -28,12 +36,12 @@ makePlots = function(
   sids = as.character(unique(records$sid))
 
   # check if sids available are compatible with the combs ::TODO::
-  # stopifnot(combs %in% sids)
-
-
-  # check which sensors are available in records & instancing the sensors list
-  records_sensors = colnames(records)[colnames(records) %in% sensors]
   # sensors = checkSensors(records, sensors)
+  if (!missing(sensors)) {
+    records_sensors = sensors
+  } else{
+    records_sensors = colnames(records)[colnames(records) %in% c("tsa", "plu", "ens", "vvt", "hct")]
+  }
   records_sensors = as.list(records_sensors)
   names(records_sensors) = records_sensors
 
@@ -41,11 +49,13 @@ makePlots = function(
   records = records %>%
     dplyr::mutate(sid = as.factor(sid))
 
-  # setting month, date, hour columns for proper plotting
-
-
-
   getSensorPlots = function(records_sensor){
+
+    # if not combs provided, create the list of each unique sid
+    if (is.null(combs)){
+      combs = as.list(unique(records$sid))
+      names(combs) = unique(records$sid)
+    }
 
     # iterate on each comb
     lapply(combs, function(comb){
@@ -93,32 +103,41 @@ makePlots = function(
       # if comb is only 2 stations then make scatter and BA
       if (length(comb) == 2) {
 
-        # adding a color factor column for plots that depends of the time extent of the records.
-        if (length(unique(lubridate::month(records$mtime))) > 3) {
-          lubridate_TimeGroup = "month"
-        }
-        else {
-          if (length(unique(lubridate::date(records$mtime))) > 14) {
-            lubridate_TimeGroup = "week"
+        if (is.null(grouping)) {
+          # adding a color factor column for plots that depends of the time extent of the records.
+          if (length(unique(lubridate::as_date(records$mtime))) > 28) {
+            lubridate_TimeGroup = "month"
           }
           else {
-            if (length(unique(lubridate::date(records$mtime))) > 1) {
-              lubridate_TimeGroup = "date"
-            } else{
-              lubridate_TimeGroup = "hour"
+            if (length(unique(lubridate::as_date(records$mtime))) > 10) {
+              lubridate_TimeGroup = "week"
+            }
+            else {
+              if (length(unique(lubridate::as_date(records$mtime))) > 1) {
+                lubridate_TimeGroup = "as_date"
+              } else{
+                lubridate_TimeGroup = "hour"
+              }
             }
           }
+        }else {
+          lubridate_TimeGroup = grouping
         }
+
         records = records %>%
           dplyr::mutate(timeGroup = as.factor(get(lubridate_TimeGroup)(mtime)))
         colnames(records)[colnames(records) == "timeGroup"] = lubridate_TimeGroup
 
+        if (!is.na(colnames(records)["as_date"])) {
+          colnames(records)[colnames(records) == "as_date"] = "date"
+          lubridate_TimeGroup = "date"
+        }
 
         # putting in wide format for scatter & blandAltman
         recordsWide = records %>%
           dplyr::select(one_of(c("mtime", lubridate_TimeGroup, "sid", records_sensor))) %>%
           tidyr::spread_("sid", records_sensor) %>%
-          dplyr::rename_at(vars(comb),function(x) paste0("station_", x))
+          dplyr::rename_at(vars(comb),function(x) paste0("station ", x))
 
         # scatter plot
         plots$scatter = recordsWide %>%
@@ -136,10 +155,10 @@ makePlots = function(
 
         # bland-Altman plot
         blandAlt = BlandAltmanLeh::bland.altman.stats(
-          dplyr::select(recordsWide, paste0("station_", comb[1]))[[1]],
-          dplyr::select(recordsWide, paste0("station_", comb[2]))[[1]])
+          dplyr::select(recordsWide, paste0("station ", comb[1]))[[1]],
+          dplyr::select(recordsWide, paste0("station ", comb[2]))[[1]])
         blandAltDf = data.frame(means = blandAlt$means, diffs = blandAlt$diffs)
-        blandAltDf = data.frame(bind_cols(blandAltDf, recordsWide))
+        blandAltDf = data.frame(dplyr::bind_cols(blandAltDf, recordsWide))
 
         plots$blandAltman = blandAltDf %>%
           ggplot(
