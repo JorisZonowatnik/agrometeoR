a4de4df807d5564132c9e84be974320be5695533
pokyah <thomas.goossens@protonmail.com>
2019-04-26 08:41:13 +0000

    reviewing benchamrkings functions

--- R/makeBmrsExtraction.R
+++ R/makeBmrsExtraction.R
@@ -1,31 +1,85 @@
 #' @export
-#' @title analyse batch of benchmark experiments output
+#' @title Compute and extract relevant information from a batch of benchmark experiments. This extraction can then be used to produce various plots.
 #' @author Thomas Goossens
 #' @import mlr
 #' @importFrom magrittr %>%
-#' @param benchmarkResult a list of class BenchmarkResult returned in the outputs of analyzeBatchOfBenchExp
-#' @param tasks a list of the mlr taks that were passed to analyzeBatchOfBenchExp
-#' @param as.df a boolean specifying if the result must be returned as a single df rather than list of lists
-#' @return a list containing relevant performance indications by learners
+#' @param benchmarkResult an object of class \code{mlr::benchmark()} produced by \code{agrometeoR::makeBrmsBatch}
+#' @param tasks a list which elements are of class \code{mlr::makeRegrTask()}. These tasks must be the same that were passed to makeBmrsBatch
+#' @param tasks_sids a list which elements are integer vectors containing the sids of the used stations of each task. This list is provided as an output element of the \code{agrometeoR::makeTaks()} function.
+#' @param as.df a boolean specifying if the result must be returned as a single dataframe rather than list of lists. Default is \code{TRUE}
+#' @return A 2 elements named list
+#' \itemize{
+#'   \item \code{snitch} : a boolean. Is \code{TRUE} if function has provided the expected result. Is \code{FALSE} is function throws an error
+#'   \item \code{output} : a named list which elements are : \itemize{
+#'     \item \code{value} : an element of class \code{data.frame} if parameter \code{as.df} is set to \code{TRUE}. If set to \code{FALSE}, the function returns a list which elements are dataframes
+#'     \item \code{condition} : a character specifying the condition encountered by the function : success, warning, or error.
+#'     \item \code{message} : a character specifying the message relative to the condition.
+#'     }
+#'  }
+#' @examples
+#'\dontrun{
+#' # load magrittr for pipe use : %>%
+#' library(magrittr)
+#'
+#' # create the dataset
+#' myDataset = makeDataset(
+#'   dfrom = "2017-03-04T15:00:00Z",
+#'   dto = "2017-03-04T18:00:00Z",
+#'   sensor = "tsa")
+#'
+#' # extract the list of hourly sets of records
+#' myDataset = myDataset$output$value
+#'
+#' # create the tasks
+#' myTasks = purrr::map(myDataset, makeTask, target = "tsa")
+#'
+#'#' # extract the used sids of each task from the outputs
+#' myUsedSids = myTasks %>% purrr::modify_depth(1, ~.$output$stations$used)
+#'
+#' # extract the tasks from the outputs
+#' myTasks = myTasks %>% purrr::modify_depth(1, ~.$output$value$task)
+#'
+#' # Conduct a batch of benchmarks experiments without saving temp files
+#'myBmrsBatch = makeBmrsBatch(
+#' tasks = myTasks,
+#' learners = agrometeorLearners,
+#' measures = list(mlr::rmse),
+#' keep.pred = TRUE,
+#' models = FALSE,
+#' groupSize  = NULL,
+#' level = "mlr.benchmark",
+#' resamplings = "LOO",
+#' cpus = 1,
+#' prefix = NULL,
+#' temp_dir = NULL,
+#' removeTemp = FALSE)
+#'
+#' # Keep the relevant information
+#' myBmrsBatch = myBmrsBatch$output$value
+#'
+#' # Get the extraction from myBmrsBatch
+#' myBrmsExtraction = makeBmrsExtraction(myBmrsBatch, myTasks, myUsedSids, as.df = TRUE)
+#'
+#' # Get an excerpt of the output
+#' head(myBmrsExtraction$output$value)
+#' }
+
 
 makeBmrsExtraction <- function(
-  benchmark_preds,
-  benchmark_perfs,
+  benchmarkResult,
   tasks,
+  tasks_sids,
   as.df = TRUE){
 
   output = list(value = NULL, condition = list(type = NULL, message = NULL))
   snitch = FALSE
 
-  doAnalysis = function(){
+  doExtraction = function(){
     #purrr solutions inspired from slack https://r-grrr.slack.com by https://www.benjaminlouis-stat.fr/
 
-    #####
-    ## computing the residuals for each iteration of CV of each datetime of each learner
+    ## Get preds and perfs and transposing to sort by learner
     benchmark_preds = benchmark_preds %>% purrr::transpose() #transposing to sort by learners rather than by date
-    benchmark_perfs = benchmark_perfs %>% purrr::transpose()
-
-    # benchmark_aggPerfs = getBMRAggrPerformances(benchmarkResult, as.df = FALSE) %>% purrr::transpose()
+    benchmark_perfs =  benchmark_perfs %>% purrr::transpose()
 
     # computing the residuals for each set of records preds
     get_res <- function(l) {
@@ -42,113 +96,29 @@ makeBmrsExtraction <- function(
 
     # getting the sids of the stations used for each set of records
     get_used_sids = function(l){
-      used = data.frame(l$output$stations$used)
+      used = data.frame(tasks_sids)
+      # used = data.frame(l$output$stations$used)
       colnames(used) = "sid"
       return(used)
     }
 
     used_sids = tasks %>% purrr::modify_depth(1, get_used_sids)
 
-    # colbinding the sids to the residuals for later global summary
+    # colbinding the sids to the residuals
     bind_sids = function(l1, l2){
       l = l1 %>%
-        purrr::map2(l2, bind_cols)
+        purrr::map2(l2, dplyr::bind_cols)
     }
     residuals = residuals %>%
       purrr::modify_depth(1, bind_sids, used_sids)
 
-    # colbinding the perfs to the residuals for later global summary
+    # colbinding the perfs to the residuals
     bind_perfs = function(l1, l2) {
       test = purrr::map2(l1, l2, dplyr::bind_cols)
       test = purrr::map(test, ~ dplyr::select(., -c("iter1", "set")))
       return(test)
     }
     dataset = purrr::map2(benchmark_perfs, residuals, bind_perfs)
-
-    #####
-    ## globally summarizing each learner by datetime on all the stations
-    # summarize_by_datetime = function(l){
-    #   summarize = function(df){
-    #     summary = df %>%
-    #       dplyr::summarise(
-    #         min_obs = min(truth),
-    #         min_pred = min(response),
-    #         min_diff_op = min(truth) - min(response),
-    #         min_ratio_op = min(truth)/min(response),
-    #         max_obs = max(truth),
-    #         max_pred = max(response),
-    #         max_diff_op = max(truth) - max(response),
-    #         max_ratio_op = max(truth)/max(response),
-    #         mean_obs = mean(truth),
-    #         mean_pred = mean(response),
-    #         var_obs = var(truth),
-    #         var_pred = var(response),
-    #         stdev_obs = sd(truth),
-    #         stdev_pred = sd(response),
-    #         min_rmse = min(rmse),
-    #         max_rmse = max(rmse),
-    #         mean_rmse = mean(rmse),
-    #         min_abs_res = mean(abs(residuals), na.rm = TRUE),
-    #         max_abs_res = max(abs(residuals), na.rm = TRUE),
-    #         mean_abs_res = min(abs(residuals), na.rm = TRUE),
-    #         mean_se = mean(se, na.rm = TRUE))
-    #   }
-    #   summarizes = l %>%
-    #     purrr::map(summarize)
-    #   summarizes.df = map2_df(summarizes, names(summarizes), ~purrr::update_list(.x, datetime = .y))
-    #   return(summarizes.df)
-    # }
-    # summarized_by_datetime = dataset %>%
-    #   purrr::modify_depth(1, summarize_by_datetime)
-
-
-    #####
-    ## globally summarizing each learner by stations on all the datetimes
-    # transpose_sid_date = function(l){
-    #   l =  purrr::map2(l, names(l), ~purrr::update_list(.x, datetime = .y))
-    #   l = l %>%
-    #     dplyr::bind_rows() %>%
-    #     dplyr::group_split(sid, keep = TRUE)
-    #   names(l) = purrr::map(l, ~unique(.$sid))
-    #   return(l)
-    # }
-    #
-    # dataset_by_sid = dataset %>%
-    #   purrr::modify_depth(1, transpose_sid_date)
-    # summarize_by_sid = function(l){
-    #   summarize = function(df){
-    #     summary = df %>%
-    #       dplyr::group_by(sid) %>%
-    #       dplyr::summarise(
-    #         min_obs = min(truth),
-    #         min_pred = min(response),
-    #         min_diff_op = min(truth) - min(response),
-    #         min_ratio_op = min(truth)/min(response),
-    #         max_obs = max(truth),
-    #         max_pred = max(response),
-    #         max_diff_op = max(truth) - max(response),
-    #         max_ratio_op = max(truth)/max(response),
-    #         mean_obs = mean(truth),
-    #         mean_pred = mean(response),
-    #         var_obs = var(truth),
-    #         var_pred = var(response),
-    #         stdev_obs = sd(truth),
-    #         stdev_pred = sd(response),
-    #         min_rmse = min(rmse),
-    #         max_rmse = max(rmse),
-    #         mean_rmse = mean(rmse),
-    #         min_abs_res = mean(abs(residuals), na.rm = TRUE),
-    #         max_abs_res = max(abs(residuals), na.rm = TRUE),
-    #         mean_abs_res = min(abs(residuals), na.rm = TRUE),
-    #         mean_se = mean(se, na.rm = TRUE))
-    #   }
-    #   summarizes = l %>%
-    #     purrr::map(summarize)
-    #   summarizes.df = purrr::map2_df(summarizes, names(summarizes), ~purrr::update_list(.x, sid = .y))
-    #   return(summarizes.df)
-    # }
-    # summarized_by_sid = dataset_by_sid %>%
-    #   purrr::modify_depth(1, summarize_by_sid)
 
     #####
     ## making a single big dataframe for plots
@@ -165,34 +135,29 @@ makeBmrsExtraction <- function(
     # Throw a success message
     message("Success, Analysis of batch of benchmark experiments performed")
 
-    #####
-    ##return all the analysis results in a list
-    analysis = list(
-      dataset = dataset#,
-      # summarized_by_sid = summarized_by_sid,
-      # summarized_by_datetime = summarized_by_datetime
-    )
+    # return the result
+    return(dataset)
 
   }
 
   tryCatch(
     expr = {
 
+      benchmark_preds = mlr::getBMRPredictions(benchmarkResult)
+      benchmark_perfs =  mlr::getBMRPerformances(benchmarkResult)
 
-      # in case the names of bmrs and tasks do not correspond, lets filter to only keep the matching items
-      benchmark_perfs = benchmark_perfs %>% purrr::keep(., names(.) %in% names(tasks))
-      benchmark_preds = benchmark_preds %>% purrr::keep(., names(.) %in% names(tasks))
-      tasks = tasks %>% purrr::keep(., names(.) %in% names(benchmark_perfs))
-      warning(paste0(
-        "Some of your tasks do not have a corresponding benchmark result. "
-      ))
+      if (!identical(names(tasks), getBMRTaskIds(benchmarkResult))){
+        tasks = tasks %>% purrr::keep(., names(.) %in% names(getBMRTaskIds(benchmarkResult)))
 
+        # benchmark_perfs = benchmark_perfs %>% purrr::keep(., names(.) %in% names(tasks))
+        # benchmark_preds = benchmark_preds %>% purrr::keep(., names(.) %in% names(tasks))
+        warning(paste0(
+          "Some of your tasks do not have a corresponding benchmark result. Only the matching ones have been kept"
+        ))
+      }
 
-      # check if passed tasks and bmr outputs are corresponding
-      stopifnot(identical(getBMRTaskIds(benchmarkResult), names(tasks)))
-
-      # in case everything went fine, do analyzeBatchOfBenchExp
-      output$value = doAnalysis()
+      # in case everything went fine, doExtraction
+      output$value = doExtraction()
       output$condition$type = "success"
       output$condition$message = "Analysis performed"
       snitch = TRUE
@@ -203,7 +168,7 @@ makeBmrsExtraction <- function(
         "AgrometeoR::analyzeBatchOfBenchExp raised a warning -> ",
         w)
       snitch <<- TRUE
-      output$value <<- doAnalysis()
+      output$value <<- doExtraction()
       output$condition$type <<- "warning"
       output$condition$message <<- warning
     },
